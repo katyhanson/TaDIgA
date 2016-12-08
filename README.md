4b6e042c4bc8ab707500b8b94725698399380b2b
John T. Foster <johntfosterjr@gmail.com>
2016-12-07 22:12:26 -0600

    First successfull compile

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-**T**orque **A**nd **D**rag **I**so**g**eometric **A**nalysis
+**T**orque **a**nd **D**rag **I**so**g**eometric **A**nalysis
 
 More details to come...
 
--- scripts/do-configure-macos.sh
+++ scripts/do-configure-macos.sh
@@ -1,6 +1,8 @@
-/usr/bin/env bash
+#!/usr/bin/env bash
+
+MPI_PATH=/usr/local/openmpi/bin
 
 cmake -DTrilinos_DIR:PATH=/usr/local/trilinos/include \
-      -DCMAKE_CXX_COMPILER:STRING=mpicc \
-      -DCMAKE_CXX_FLAGS:STRING="-std=c++14" \
+      -DCMAKE_CXX_COMPILER:PATH=$MPI_PATH/mpicxx \
+      -DCMAKE_CXX_FLAGS:STRING="-std=c++14 -O2" \
       ..
