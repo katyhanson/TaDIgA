a5cf2d5ad55c1191959a5c99f37c03ae4bad6011
John T. Foster <johntfosterjr@gmail.com>
2016-12-16 08:41:09 -0600

    Add IGES read class

---
+++ src/tadiga_IGES_geometry.h
@@ -0,0 +1,45 @@
+// Copyright 2015 John T. Foster
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//    http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#ifndef TADIGA_IGES_GEOMETRY_HPP
+#define TADIGA_IGES_GEOMETRY_HPP
+
+#include "tadiga_geometry.h"
+
+namespace tadiga {
+
+//! Geometry class that reads an IGES file
+class IGESGeometry : public tadiga::Geometry {
+   public:
+    //! Constructor
+    IGESGeometry(
+        const Teuchos::RCP<const Epetra_Comm>& kComm,
+        const Teuchos::RCP<Teuchos::ParameterList>& kGeometryParameters);
+
+    //! Destructor
+    virtual ~IGESGeometry();
+
+   private:
+    //! Private to prohibit copying
+    IGESGeometry(const IGESGeometry&);
+
+    //! Private to prohibit copying
+    IGESGeometry& operator=(const IGESGeometry&);
+
+   protected:
+    //! Epetra communicator
+    Teuchos::RCP<const Teuchos::Comm<int>>& kComm;
+};
+}
+
+#endif  // TADIGA_IGES_GEOMETRY_HPP
