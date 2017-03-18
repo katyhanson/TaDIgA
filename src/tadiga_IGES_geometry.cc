ea1de24b5fd46e396b35da55a57467f4c62fa03b
Katy Hanson <katyhanson.16@gmail.com>
2017-03-18 18:06:30 -0500

    Commit

--- src/tadiga_IGES_geometry.cc
+++ src/tadiga_IGES_geometry.cc
@@ -12,18 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-<<<<<<< HEAD
-=======
-#include "IGESControl_Reader.hxx"
-#include "TColStd_HSequenceOfTransient.hxx"
-#include "TopoDS_Edge.hxx"
-#include "TopoDS_Face.hxx"
-#include "TopoDS_Shape.hxx"
-#include "TopoDS_Vertex.hxx"
-
-#include "Teuchos_RCP.hpp"
-
->>>>>>> iges
 #include "tadiga_IGES_geometry.h"
 #include "IGESControl_Reader.hxx"
 #include "TColStd_HSequenceOfTransient.hxx"
@@ -57,8 +45,7 @@ tadiga::IgesGeometry::IgesGeometry(
     const auto status = kIgesReader->ReadFile(kFileName.c_str());
 
     // TODO(johntfoster@gmail.com): Check the status of the file
-<<<<<<< HEAD
-
+    //
     // Returns list of all ntities from IGES file
     Handle(TColStd_HSequenceOfTransient) igesEntityList =
         kIgesReader->GiveList();
@@ -68,90 +55,4 @@ tadiga::IgesGeometry::IgesGeometry(
 
     // Obtain result in a single OCCT shape
     TopoDS_Shape transferred_OCCT_shape = kIgesReader->OneShape();
-
-    // Convert transferred OCCT Shapes to NURBS
-    const auto kNURBSConverter = Teuchos::rcp(new BRepBuilderAPI_NurbsConvert);
-    kNURBSConverter->Perform(transferred_OCCT_shape);
-
-    // Retrieve modified NURBS converted transferred_OCCT_shape
-    TopoDS_Shape NURBS_converted_shape =
-        kNURBSConverter->ModifiedShape(transferred_OCCT_shape);
-
-    // Look for ToopDS_Edges in  NURBS_converted_shape
-    TopExp_Explorer ShapeExplorer;
-    for (ShapeExplorer.Init(NURBS_converted_shape, TopAbs_EDGE);
-         ShapeExplorer.More(); ShapeExplorer.Next()) {
-        // Convert found to type  TopoDS_Edge
-        const TopoDS_Edge& ExtractedEdge =
-            TopoDS::Edge(ShapeExplorer.Current());
-        // Create Adaptor BRep Curve to Read Geometry of Extracted TopoDS_Edge
-        const auto BRepAdaptorCurve =
-            Teuchos::rcp(new BRepAdaptor_Curve(ExtractedEdge));
-        // Use Adaptor BRep Curve to Read BSpline
-        Handle(Geom_BSplineCurve) ExtractedBSplineCurve =
-            BRepAdaptorCurve->BSpline();
-        // Extract Knot Sequence from BSpline Extracted with the Explorer from
-        // the NURBS_converted_shape
-        TColStd_Array1OfReal knot_sequence = ExtractedBSplineCurve->Knots();
-    }
-
-    // Look for TopoDS_Faces in NURBS_converted_shape
-    for (ShapeExplorer.Init(NURBS_converted_shape, TopAbs_FACE);
-         ShapeExplorer.More(); ShapeExplorer.Next()) {
-        // Convert found to type  TopoDS_Face
-        const TopoDS_Face& ExtractedFace =
-            TopoDS::Face(ShapeExplorer.Current());
-        // Create Adaptor BRep Curve to Read Geometry of Extracted
-        // TopoDS_Face
-        const auto BRepAdaptorSurface =
-            Teuchos::rcp(new BRepAdaptor_Surface(ExtractedFace));
-        // Use Adaptor BRep Suface to Read BSplineSurface
-        Handle(Geom_BSplineSurface) ExtractedBSplineSurface =
-            BRepAdaptorSurface->BSpline();
-        // Extract Knot Sequence (U and V surface dimensions) from BSpline
-        // Extracted from
-        // NURBS_converted_shape
-        TColStd_Array1OfReal U_knot_sequence =
-            ExtractedBSplineSurface->UKnotSequence();
-        TColStd_Array1OfReal V_knot_sequence =
-            ExtractedBSplineSurface->VKnotSequence();
-    }
-=======
-    // selects all IGES faces in the file and puts them into a list  called
-    // MyList; Returns list of entities from IGES file
-
-    Handle(TColStd_HSequenceOfTransient) myFaceList =
-        kIgesReader->GiveList("iges-faces");
-    Handle(TColStd_HSequenceOfTransient) myLineList =
-        kIgesReader->GiveList("iges-type(110)");
-    Handle(TColStd_HSequenceOfTransient) myTabulatedCylinderList =
-        kIgesReader->GiveList("iges-type(122)");
-    Handle(TColStd_HSequenceOfTransient) myCompositeCurveList =
-        kIgesReader->GiveList("iges-type(102)");
-    Handle(TColStd_HSequenceOfTransient) myCurveOnSurfaceList =
-        kIgesReader->GiveList("iges-type(142)");
-    Handle(TColStd_HSequenceOfTransient) myBSplineCurveList =
-        kIgesReader->GiveList("iges-type(126)");
-
-    number_of_iges_face_ = myFaceList->Length();
-    number_of_transferred_face_ = kIgesReader->TransferList(myFaceList);
-
-    number_of_iges_line_ = myLineList->Length();
-    number_of_transferred_line_ = kIgesReader->TransferList(myLineList);
-
-    number_of_iges_tabulated_cylinder_ = myTabulatedCylinderList->Length();
-    number_of_transferred_tabulated_cylinder_ =
-        kIgesReader->TransferList(myTabulatedCylinderList);
-
-    number_of_iges_composite_curve_ = myCompositeCurveList->Length();
-    number_of_transferred_composite_curve_ =
-        kIgesReader->TransferList(myCompositeCurveList);
-
-    number_of_iges_curve_on_surface_ = myCurveOnSurfaceList->Length();
-    number_of_transferred_curve_on_surface_ =
-        kIgesReader->TransferList(myCurveOnSurfaceList);
-
-    TopoDS_Shape transfered_OCCT_shape = kIgesReader->OneShape();
-    // Obtains the results in a single OCCT shape
->>>>>>> iges
 };
